3494eee7e45c04f8cf15d9e7dc6832ef0a3819fe
Manuel Alvarez <mrmanuelalvarez@gmail.com>
2024-10-22 02:42:14 -0300

    mejorando test de resta

--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -3,8 +3,16 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.Random;
+
 public class CalculadoraTest {
     private Calculadora calc;
+
+    public int randomIntCalc(int origin, int bound){
+        Random r = new Random();
+        int entero = r.nextInt(origin, bound);
+        return entero;
+    }
 
     @BeforeMethod
     public void setUp() {
@@ -14,14 +22,39 @@ public class CalculadoraTest {
 
     @Test
     public void testSuma() {
-        int resultado = calc.sumar(5, 3);
-        Assert.assertEquals(resultado, 8, "La suma de 5 y 3 debe ser 8");
+        int resultado = calc.sumar(randomIntCalc(0, 100), randomIntCalc(0, 100));
+        //Assert.assertEquals(resultado, 8, "La suma de 5 y 3 debe ser 8");
+        Assert.assertTrue(resultado > 0 && resultado < 199, "La suma debe ser positiva y menor a 199");
+    }
+
+    @Test
+    public void testSumaNegativa() {
+        int param1 = randomIntCalc(-100, 0);
+        int param2 = randomIntCalc(-100, 0);
+        int resultado = calc.sumar(param1, param2);
+        //int resultado = calc.restar(randomIntCalc(-100, 0), randomIntCalc(-100, 0));
+        Assert.assertTrue(resultado < 0 && resultado >= -200, "La suma debe ser negativa y mayor o igual a -200");
     }
 
     @Test
     public void testResta() {
-        int resultado = calc.restar(10, 4);
-        Assert.assertEquals(resultado, 6, "La resta de 10 y 4 debe ser 6");
+        int param1 = 6;
+        int param2 = 3;
+        int resultado = calc.restar(param1, param2);
+
+        Assert.assertTrue(resultado < param1, "El resultado de la resta es menor al primer número");
+
+        param2 = 0;
+        resultado = calc.restar(param1, param2);
+        Assert.assertEquals(resultado, param1, "El resultado es igual al primer número");
+
+        param1 = 0;
+        resultado = calc.restar(param1, param2);
+        Assert.assertEquals(resultado, 0, "El resultado debe dar 0");
+
+        param2 = 2;
+        resultado = calc.restar(param1, param2);
+        Assert.assertTrue(resultado < 0, "El resultado de la resta debe ser negativo");
     }
 
     // Este test depende de que testSuma se ejecute y pase
@@ -29,5 +62,11 @@ public class CalculadoraTest {
     public void testSumaConDependencia() {
         int resultado = calc.sumar(2, 2);
         Assert.assertEquals(resultado, 4, "La suma de 2 y 2 debe ser 4");
+    }
+
+    @Test(dependsOnMethods = {"testResta"})
+    public void testRestaConDependencia() {
+        int resultado = calc.restar(2, 2);
+        Assert.assertEquals(resultado, 0, "La resta de 2 y 2 debe ser 0");
     }
 }
